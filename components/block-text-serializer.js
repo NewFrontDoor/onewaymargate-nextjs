2591796b0bd8faab40d04bb2d082fffc5a10000c
readeral <alan@readeral.com>
2020-03-12 21:03:03 +1100

    adding form validation

--- components/block-text-serializer.js
+++ components/block-text-serializer.js
@@ -76,6 +76,24 @@ const FormSerializer = ({node}) => {
   return (
     <Form
       {...node}
+      validations={values => {
+        const errors = {};
+        node.fields.forEach(field => {
+          const rg = field.regex?.regexString
+            ? new RegExp(field.regex.regexString, 'i')
+            : false;
+          if (field.required) {
+            if (!values[field.id]) {
+              errors[field.id] = 'Required';
+            } else if (rg && !rg.test(values[field.id])) {
+              errors[field.id] = field.regex.warning;
+            }
+          } else if (rg && values[field.id] && !rg.test(values[field.id])) {
+            errors[field.id] = field.regex.warning;
+          }
+        });
+        return errors;
+      }}
       blockText={val => <BlockText blocks={val} />}
       submitForm={() => console.log('submitted!')}
     />
